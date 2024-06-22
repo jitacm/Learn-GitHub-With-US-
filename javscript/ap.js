55d60f46644b1e4446db845750926872435c109b
animex007 <ayushjagdhane7@gmail.com>
2024-06-22 21:36:18 +0530

    add some changes

---
+++ javscript/ap.js
@@ -0,0 +1,30 @@
+// This is a basic JavaScript script
+
+// Declare a variable
+let message = "Hello, world!";
+
+// Print the message to the console
+console.log(message);
+
+// Perform some basic arithmetic
+let x = 5;
+let y = 10;
+let sum = x + y;
+console.log("The sum of x and y is: " + sum);
+
+// Use conditional statements
+if (x > y) {
+    console.log("x is greater than y");
+} else if (x < y) {
+    console.log("x is less than y");
+} else {
+    console.log("x is equal to y");
+}
+
+// Create a function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Call the function
+greet("Alice");
